52976aac8666018d749c6f512487018c34d8f10a
smorad993 <shahram.moradi35@gmail.com>
2026-03-24 06:50:04 -0400

    revert

--- main.tf
+++ main.tf
@@ -1,16 +1,22 @@
 data "aws_ami" "app_ami" {
   most_recent = true
-  owners      = ["amazon"]
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
   }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["979382823631"] # Bitnami
 }
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
-  instance_type = "t2.micro"
+  instance_type = "t3.nano"
 
   tags = {
     Name = "HelloWorld"
