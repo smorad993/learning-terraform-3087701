cc7406125d9c5d2d965cbae4b64df1bea3f7aeea
smorad993 <shahram.moradi35@gmail.com>
2026-03-24 05:50:10 -0400

    amzonfile

--- main.tf
+++ main.tf
@@ -1,23 +1,16 @@
 data "aws_ami" "app_ami" {
   most_recent = true
-  owner       =["amazon"]
+  owners      = ["amazon"]
 
   filter {
     name   = "name"
     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["979382823631"] # Bitnami
 }
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
-  instance_type = "t2.micro"    #change because of free account t3 to t2
+  instance_type = "t2.micro"
 
   tags = {
     Name = "HelloWorld"
