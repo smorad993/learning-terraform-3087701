49d7cc71dbe2371f1333c872a1dc0f013e8c71d3
smorad993 <shahram.moradi35@gmail.com>
2026-03-24 05:18:25 -0400

    t3 to t2

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ data "aws_ami" "app_ami" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
+  instance_type = "t2.micro"    #change because of free account t3 to t2
 
   tags = {
     Name = "HelloWorld"
